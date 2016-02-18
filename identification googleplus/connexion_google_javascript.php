bbd997fa33a21ca474ec490ad8dbf9b95d4a85f1
bastv212 <osef@nananere.com>
2016-02-18 19:33:49 +0100

    blat

--- identification googleplus/connexion_google_javascript.php
+++ identification googleplus/connexion_google_javascript.php
@@ -1,27 +1,24 @@
-<!-- source :
+<!-- script pour se connecter via gogole EN JS
+ source :
  https://developers.google.com/identity/sign-in/web/devconsole-project
  * créer un "projet" sur https://console.developers.google.com pour choper un "ID client",
    configurer ("gestionnaire d'apis > identifiants") le nom et l'origine JS autorisées (domaine du site où
    se trouve la page de connexion) 
-   ("URI de redirection autorisés" = pas utile avec le login JS)
-     
+   ("URI de redirection autorisés" = pas utile avec le login JS)   
    
+   (ici client id : 22946272988-a3e2srpeacg8apvl36g5ii09sh4uea89.apps.googleusercontent.com)
 --> 
 
-
+<head>
 <meta name="google-signin-client_id" content="22946272988-a3e2srpeacg8apvl36g5ii09sh4uea89.apps.googleusercontent.com">
 <script src="https://apis.google.com/js/platform.js" async defer></script>
+</head>
 
-
-
-<?php
-	// client id
-	//  22946272988-a3e2srpeacg8apvl36g5ii09sh4uea89.apps.googleusercontent.com 
-?>
+<body>
 
 <div class="g-signin2" data-onsuccess="onSignIn"></div>
-
 <script>
+// fct qui s'exécute quand la connexion est faite
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -31,8 +28,8 @@ function onSignIn(googleUser) {
 }
 </script>
 
-
 <!-- bouton et script pour se déconnecter -->
+
 <a href="#" onclick="signOut();">Sign out</a>
 <script>
   function signOut() {
@@ -42,3 +39,4 @@ function onSignIn(googleUser) {
     });
   }
 </script>
+
